8ed2eb058ed916f64b17f1c144d1c4bd45e11ae7
henil1711 <henilkapadiya90@gmail.com>
2023-01-19 15:12:08 +0530

    modul 6

---
+++ friendfuction.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+class Swap
+{
+
+    int num;
+
+public:
+    Swap(int num)
+    {
+        this->num = num;
+    }
+
+    friend void swap(Swap &, Swap &);
+};
+
+void swap(Swap &s1, Swap &s2)
+{
+    int temp;
+    cout << "\nBefore Swapping: " << s1.num << " " << s2.num;
+
+    temp = s1.num;
+    s1.num = s2.num;
+    s2.num = temp;
+    cout << "\nAfter Swapping: " << s1.num << " " << s2.num;
+}
+
+int main()
+{
+    Swap s1(6), s2(10);
+    swap(s1, s2);
+    return 0;
+}
